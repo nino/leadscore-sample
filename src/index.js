adbc955c96578a1c7cd5cc3abafeec73a1b70b51
Nino Annighöfer <nino@ninoan.com>
2017-04-24 13:29:13 +0200

    Set up redux
    Note: Since there isn't a valid redux-store with reducers yet, the code
    will currently output an error to the console when opening the app.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 import App from './App';
 import './index.css';
+import reducer from './store';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ // eslint-disable-line no-underscore-dangle
+  && window.__REDUX_DEVTOOLS_EXTENSION__(), // eslint-disable-line no-underscore-dangle
+  applyMiddleware(sagaMiddleware),
+);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}><App /></Provider>,
   document.getElementById('root'),
 );
 
@@ -12,7 +24,7 @@ if (module.hot) {
   module.hot.accept('./App', () => {
     const NextApp = require('./App').default;
     ReactDOM.render(
-      <NextApp />,
+      <Provider store={store}><NextApp /></Provider>,
       document.getElementById('root'),
     );
   });
