e39dc47959140326bb0afa55fc1f5575b812803d
Nino Annighöfer <nino@ninoan.com>
2017-04-25 15:34:55 +0200

    Temporarily remove tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,1 @@
-/* @flow */
-/* eslint-env jest */
-/* global document */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
+/* TODO add tests for routes */
